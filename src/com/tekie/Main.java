50f26b0b7527005f5092d6caeb309e63366b6fb2
javatekie <iamjavatekie@gmail.com>
2025-07-02 21:29:41 +0530

    method is deleted

--- src/com/tekie/Main.java
+++ src/com/tekie/Main.java
@@ -1,10 +1,7 @@
 package com.tekie;
 
 public class Main {
-	private String getName() {
-		System.out.println("Hi, venkatkagital");
-		return "";
-	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello main..");
 	}
