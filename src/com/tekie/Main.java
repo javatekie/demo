801fc265e5952dbce3cd69fe889f3cb5bccecc6a
javatekie <iamjavatekie@gmail.com>
2025-07-02 22:12:30 +0530

    changes the main

--- src/com/tekie/Main.java
+++ src/com/tekie/Main.java
@@ -3,8 +3,11 @@ package com.tekie;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello main..");
-		System.out.println("syso-2 added");
+		/*
+		 * System.out.println("Hello main.."); System.out.println("syso-2 added");
+		 */
+		
+		System.out.println("Hi venkatkagital");
 	}
 
 }
