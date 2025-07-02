dd242cc0d79a17ada22d444dbace874549805b0c
javatekie <iamjavatekie@gmail.com>
2025-07-02 21:19:34 +0530

    getName() added

--- src/com/tekie/Main.java
+++ src/com/tekie/Main.java
@@ -1,6 +1,10 @@
 package com.tekie;
 
 public class Main {
+	private String getName() {
+		System.out.println("Hi, venkatkagital");
+		return "";
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello main..");
 	}
