684984268d2b1db70bb745155e05307fc572b23b
javatekie <iamjavatekie@gmail.com>
2025-07-02 22:13:27 +0530

    Update Main.java

--- src/com/tekie/Main.java
+++ src/com/tekie/Main.java
@@ -3,10 +3,7 @@ package com.tekie;
 public class Main {
 
 	public static void main(String[] args) {
-		/*
-		 * System.out.println("Hello main.."); System.out.println("syso-2 added");
-		 */
-		
+			
 		System.out.println("Hi venkatkagital");
 	}
 
